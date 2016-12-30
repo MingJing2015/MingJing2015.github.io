937579015a8e12ec21bafea2435f6f50d9c374c2
Ming <mjing3@my.bcit.ca>
2016-12-29 17:57:10 -0800

    Add files via upload
    good job !

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,3],{440:function(n,o){},988:function(n,o,c){n.exports=c(440)}},[988]);
+webpackJsonp([1,3],{1001:function(n,o,c){n.exports=c(444)},444:function(n,o){}},[1001]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
